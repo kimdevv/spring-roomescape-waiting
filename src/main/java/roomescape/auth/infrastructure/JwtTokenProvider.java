f0bbeab7419fac2f77993f13f7146796a56b8794
kimdev <kim__dev@hufs.ac.kr>
2025-05-18 19:16:05 +0900

    refactor: JWT sign 로직 리팩토링

--- src/main/java/roomescape/auth/infrastructure/JwtTokenProvider.java
+++ src/main/java/roomescape/auth/infrastructure/JwtTokenProvider.java
@@ -4,13 +4,15 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.auth.exception.InvalidTokenException;
 import roomescape.auth.model.Principal;
 import roomescape.member.model.Member;
 
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -21,27 +23,41 @@ public class JwtTokenProvider {
 
     @Value("${auth.jwt.secret-key}")
     private String secretKey;
+    @Value("${auth.jwt.signing-algorithm}")
+    private String signingAlgorithm;
     @Value("${auth.jwt.validity-ms}")
     private long validityInMilliseconds;
 
     public String createToken(Member member) {
-        Claims claims = Jwts.claims().setSubject(String.valueOf(member.getId()));
-        claims.put(MEMBER_NAME_KEY, member.getName());
-        claims.put(IS_ADMIN_KEY, member.isAdmin());
+        Key key = generateSigningKey();
+        Claims claims = generateClaims(member);
         Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds);
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setExpiration(new Date(now.getTime() + validityInMilliseconds))
+                .signWith(key)
                 .compact();
+    }
+
+    private Key generateSigningKey() {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return new SecretKeySpec(keyBytes, signingAlgorithm);
+    }
+
+    private Claims generateClaims(Member member) {
+        Claims claims = Jwts.claims().setSubject(String.valueOf(member.getId()));
+        claims.put(MEMBER_NAME_KEY, member.getName());
+        claims.put(IS_ADMIN_KEY, member.isAdmin());
+        return claims;
     }
 
     public Principal resolvePrincipalFromToken(String token) {
         try {
-            Claims claims = Jwts.parser()
-                    .setSigningKey(secretKey)
+            Key key = generateSigningKey();
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
                     .parseClaimsJws(token)
                     .getBody();
             Long memberId = Long.valueOf(claims.getSubject());
